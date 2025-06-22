9dde3e776fd8316991b425daae458be73d18dee0
BangSaM0607 <khisamsimo0607@gmail.com>
2025-06-22 20:51:34 +0700

    Memperbarui inisialisasi Supabase dan menyesuaikan tema aplikasi dengan Material 3, serta menambahkan logika untuk menampilkan halaman login atau beranda berdasarkan status sesi pengguna.

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://lleiuchgukmblykhhduz.supabase.co',
+    url:
+        'https://lleiuchgukmblykhhduz.supabase.co', // ganti dengan URL project kamu
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsZWl1Y2hndWttYmx5a2hoZHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MjU1MzYsImV4cCI6MjA2MzIwMTUzNn0.TolJDNpew7JVGe2tP9ngx3BHR97LNNvyZNTALE-cDT8',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsZWl1Y2hndWttYmx5a2hoZHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MjU1MzYsImV4cCI6MjA2MzIwMTUzNn0.TolJDNpew7JVGe2tP9ngx3BHR97LNNvyZNTALE-cDT8', // ganti dengan anon key project kamu
   );
-  runApp(MyApp());
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,10 +21,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final session = Supabase.instance.client.auth.currentSession;
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CCTV Management',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: session == null ? const LoginPage() : const HomePage(),
     );
   }
 }
